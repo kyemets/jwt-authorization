699e9996678bf3301051a83a3b7908b797143382
kyemets <yemetskonstantin@gmail.com>
2023-05-14 22:53:15 +0200

    fix create token

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,15 +1,20 @@
 const userService = require('../service/user-service');
+const {validationResult} = require('express-validator');
+
 
 class UserController {
     async registration(req, res, next) {
         try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Ошибка при валидации', errors.array()))
+            }
             const {email, password} = req.body;
             const userData = await userService.registration(email, password);
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true});
-
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
             return res.json(userData);
         } catch (e) {
-            console.log(e);
+            next(e);
         }
     }
 
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -2,32 +2,27 @@ const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
-const tokenService = ('./token-service.js')
+const tokenService = require('./token-service');
 const UserDto = require('../dtos/user-dto');
+// const ApiError = require('../exceptions/api-error');
 
 class UserService {
     async registration(email, password) {
         const candidate = await UserModel.findOne({email})
         if (candidate) {
-            throw new Error(`User with email: ${email}, already registered`)
-        } 
-        const hashPassword = await bcrypt.hash(password, 3)
-        const activationLink = uuid.v4() // v34fa-asdfas-adege3-sa-sf
+            throw ApiError.BadRequest(`Пользователь с почтовым адресом ${email} уже существует`)
+        }
+        const hashPassword = await bcrypt.hash(password, 3);
+        const activationLink = uuid.v4(); // v34fa-asfasf-142saf-sa-asf
 
         const user = await UserModel.create({email, password: hashPassword, activationLink})
-        await mailService.sendActivationMail(email, activationLink)
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
 
         const userDto = new UserDto(user); // id, email, isActivated
-        const tokens = tokenService.generateTokens({...userDto})
-        await tokenService.saveToken(userDto.id, tokens.refreshToken)
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
-        
-
-
-        return {
-            ...tokens,
-            user: userDto
-        }
+        return {...tokens, user: userDto}
     }
 }
 
